cd514b9aa3d2d58e1d9dd49cef5b064db17133c7
Rishabh Jain <69865046+jennyrishabh@users.noreply.github.com>
2021-04-05 12:39:17 +0530

    Add files via upload

---
+++ updated files(with dates)/test_5april.java
@@ -0,0 +1,70 @@
+package ada_2021;
+
+
+public class test_5april {
+	
+	//19csu247
+		
+	
+	static void selectionSort(String arr[],int n)
+	{
+		
+				for(int i = 0; i < n - 1; i++)
+				{
+				
+					
+					int min_index = i;
+					String minStr = arr[i];
+					for(int j = i + 1; j < n; j++)
+					{
+						
+					
+					
+						if(arr[j].compareToIgnoreCase(minStr) < 0)
+						{
+							
+							minStr = arr[j];
+							min_index = j;
+						}
+					}
+		
+				// Swapping Part
+						
+							String temp = arr[min_index];
+							arr[min_index] = arr[i];
+							arr[i] = temp;
+						
+				}
+				
+				
+				
+				
+	}
+
+	
+	public static void main(String args[])
+	{
+		String arr[] = {"ada","cyber","ade","dbms","IOT","IPS","iots"};
+		int n = arr.length;
+			System.out.println("Given array is");
+			
+		// before sorting
+		for(int i = 0; i < n; i++)
+		{
+			System.out.print(arr[i]+" , ");
+		}
+		System.out.println("\n");
+
+		selectionSort(arr, n);
+
+		System.out.println("afert Sorted ");
+		
+		
+		for(int i = 0; i < n; i++)
+		{
+			System.out.print(arr[i]+" , ");
+		}
+	}
+	
+
+}
